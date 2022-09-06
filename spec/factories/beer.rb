05907373e8f77d873ceb9308a8f1e62f7a1fe52b
Luke Billings <lukecbillings@gmail.com>
2022-09-06 20:06:20 +0100

    json response size testing working

---
+++ spec/factories/beer.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :beer do
+ end
+end
--- spec/requests/beers_spec.rb
+++ spec/requests/beers_spec.rb
@@ -1,3 +1,14 @@
 require 'rails_helper'
 
 
+describe 'Beers API', type: :request do
+    it 'returns all beers' do
+      FactoryBot.create(:beer, identifier: 1, name: "Example Beer name" , description: "This is an example of a beer that will be returned by your API")
+      FactoryBot.create(:beer, identifier: 2, name: "Example Beer name 2" , description: "This is another example of a beer that will be returned by your API")
+      get '/api/v1/beers'
+      #checking response status is 200
+      expect(response).to have_http_status(:success)
+      #check gets back all record that are in API (using Factory Bot for test data)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+end
